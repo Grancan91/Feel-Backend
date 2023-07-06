7db7026e256c90d82890c195d9ea91e639d2cee6
Abel <abl.lopper@gmail.com>
2023-07-06 09:15:23 +0100

    router added

---
+++ api/routers/userRouter.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/userModel');
+
+// GET all users
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error('Error retrieving users:', error);
+        res.status(500).json({ error: 'Error retrieving users' });
+    }
+});
+
+// GET a specific user by ID
+router.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error('Error retrieving user:', error);
+        res.status(500).json({ error: 'Error retrieving user' });
+    }
+});
+
+// POST create a new user
+router.post('/users', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ error: 'Error creating user' });
+    }
+});
+
+// PUT update a user by ID
+router.put('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: 'Error updating user' });
+    }
+});
+
+// DELETE delete a user by ID
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        res.status(500).json({ error: 'Error deleting user' });
+    }
+});
+
+module.export = router;
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const createUser = require('./api/models/userModel')
 const connection = require('./db')
+const express = require('express');
+const app = express();
+const userRouter = require('./api/routers/userRouter');
 
 //Start Backend-Server
 const start = async () => {
     await connection()
     createUser()
+    
+    app.use('/api', userRouter);
+    // Other middleware and configuration...
+    app.listen(3000, () => {
+        console.log('Waiting requests')
+    })
 }
+
 start()
+
 
 
