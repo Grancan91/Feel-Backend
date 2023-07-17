77173b5aa975e25ba538ed6af6d8fa9631598ca5
Grancan91 <abl.lopper@gmail.com>
2023-07-17 16:16:35 +0100

    Endpoint for Patient Record done

--- api/controllers/record_controller.js
+++ api/controllers/record_controller.js
@@ -31,12 +31,15 @@ const createUserRecord = async (req, res) => {
 //Load all Records of Signed up User
 const loadUserRecords = async (req, res) => {
     try {
+      
         //Load Signed up user.ç
+        console.log(res.locals.user.id)
         const user = await User.findById(res.locals.user.id) // res.locals.user from checkAuth
-        console.log('loaduserRecords')
+        console.log(user)
 
         //Records of user
         const recordsId = user.records
+        console.log(user.records)
         //Load Records associated to user
         const record = await Record.find({ _id: {$in: recordsId} })
         .populate("emotions")
@@ -53,6 +56,39 @@ const loadUserRecords = async (req, res) => {
     } catch (error) {
         return res.status(500).json({ error: 'Error in loadUserRecords' });
     }
+}
+
+//Load all Records of Signed up User
+const loadPatientRecords = async (req, res) => {
+  try {
+    console.log('hola loadpatient')
+    //Load Signed up user.ç
+    console.log('req.params: ', req.params.patientId)
+    const userId = req.params.patientId
+    console.log(userId)
+    const user = await User.findById(userId) // res.locals.user from checkAuth
+
+    console.log('ostras',user)
+
+    //Records of user
+    const recordsId = user.records
+    console.log(user.records)
+    //Load Records associated to user
+    const record = await Record.find({ _id: { $in: recordsId } })
+      .populate("emotions")
+      .populate("causes")
+      .populate("symptoms")
+      .populate("strategies")
+
+
+    if (record) {
+      return res.status(200).json(record);
+    } else {
+      return res.status(400).send(`No records found for ${res.locals.user.email} user`)
+    }
+  } catch (error) {
+    return res.status(500).json({ error: 'Error in loadUserRecords' });
+  }
 }
 
 
@@ -165,6 +201,94 @@ const calculateAverageEmotions = async (req, res) => {
   }
 };
 
+const calculateAverageEmotionsPatient = async (req, res) => {
+  try {
+
+    const userId = req.params.patientId
+    //Load Signed up user.ç
+    const user = await User.findById(userId) // res.locals.user from checkAuth
+    console.log('loaduserRecords')
+
+    //Records of user
+    const recordsId = user.records
+    //Load Records associated to user
+    const records = await Record.find({ _id: { $in: recordsId } })
+      .populate("emotions")
+      .populate("causes")
+      .populate("symptoms")
+      .populate("strategies")
+    const emotionCounts = {};
+    const causeCounts = {};
+    const strategyCounts = {};
 
 
-module.exports = { createUserRecord, loadUserRecords, deleteUserRecord, calculateAverageEmotions }
+    // Contar la frecuencia de aparición de cada emoción, causa y estrategia en los registros
+    records.forEach((record) => {
+      record.emotions.forEach((emotion) => {
+        emotionCounts[emotion.name] = (emotionCounts[emotion.name] || 0) + 1;
+      });
+
+      record.causes.forEach((cause) => {
+        causeCounts[cause.name] = (causeCounts[cause.name] || 0) + 1;
+      });
+
+      record.strategies.forEach((strategy) => {
+        strategyCounts[strategy.name] = (strategyCounts[strategy.name] || 0) + 1;
+      });
+    });
+
+    // Ordenar las emociones, causas y estrategias según su frecuencia de aparición en orden descendente
+    const sortedEmotions = Object.keys(emotionCounts).sort(
+      (emotionA, emotionB) => emotionCounts[emotionB] - emotionCounts[emotionA]
+    );
+    const sortedCauses = Object.keys(causeCounts).sort(
+      (causeA, causeB) => causeCounts[causeB] - causeCounts[causeA]
+    );
+    const sortedStrategies = Object.keys(strategyCounts).sort(
+      (strategyA, strategyB) => strategyCounts[strategyB] - strategyCounts[strategyA]
+    );
+
+    // Obtener las tres emociones, causas y estrategias más recurrentes
+    const topEmotions = sortedEmotions.slice(0, 3);
+    const topCauses = sortedCauses.slice(0, 3);
+    const topStrategies = sortedStrategies.slice(0, 3);
+    const recordDetails = records.map((record) => record.detail);
+
+    // Calcular el total de registros
+    const totalRecords = records.length;
+
+    // Calcular la media de aparición de las emociones, causas y estrategias más recurrentes
+    const averageData = {
+      emotions: {},
+      causes: {},
+      strategies: {},
+      details: recordDetails,
+    };
+
+    topEmotions.forEach((emotion) => {
+      const frequency = emotionCounts[emotion] / totalRecords;
+      const percentage = (frequency * 100).toFixed(1);
+      averageData.emotions[emotion] = `${percentage}%`;
+    });
+
+    topCauses.forEach((cause) => {
+      const frequency = causeCounts[cause] / totalRecords;
+      const percentage = (frequency * 100).toFixed(1);
+      averageData.causes[cause] = `${percentage}%`;
+    });
+
+    topStrategies.forEach((strategy) => {
+      const frequency = strategyCounts[strategy] / totalRecords;
+      const percentage = (frequency * 100).toFixed(1);
+      averageData.strategies[strategy] = `${percentage}%`;
+    });
+
+    return res.status(200).json(averageData);
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: 'Error calculating average emotion' });
+  }
+};
+
+
+module.exports = { createUserRecord, loadUserRecords, deleteUserRecord, calculateAverageEmotions, calculateAverageEmotionsPatient }
--- api/routers/record_router.js
+++ api/routers/record_router.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 //Methods from controller to use in endPoints
-const { createUserRecord, loadUserRecords, deleteUserRecord, calculateAverageEmotions } = require("../controllers/record_controller");
+const { createUserRecord, loadUserRecords, deleteUserRecord, calculateAverageEmotions, calculateAverageEmotionsPatient } = require("../controllers/record_controller");
 const { checkAuth } = require("../middleware/auth");
 
 router
     // CheckAuth save user.id in res.locals
     // Get to Load All Records
     .get('/', checkAuth, loadUserRecords ) //loadUserRecords need res.locals.user
+    // Get to Load All Patients Records
+    .get('/:patientId', checkAuth, calculateAverageEmotionsPatient) //loadUserRecords need res.locals.user
     // Get Average Emotions on Records
     .get('/average', checkAuth, calculateAverageEmotions ) //calculateAverageEmotions need res.locals.user
     // Post to create Record
